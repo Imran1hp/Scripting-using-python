a6a7b27ad57a8625975b573ad38918d4b372337c
ali imran <aliimranlaskar123@gmail.com>
2025-07-21 00:40:13 +0530

    finish

--- get_game_data.py
+++ get_game_data.py
@@ -20,17 +20,41 @@ def find_all_game_paths(source):
           
      return game_paths
 
+def get_name_from_paths(paths,to_strip):
+     new_name =[]
+     for path in paths:
+          _,dir_name = os.path.split(path)
+          new_dir_name = dir_name.replace(to_strip,"")
+          new_name.append(new_dir_name)
+     return new_name
+
+
+
 def create_dir(path):
      if not os.path.exists(path):
-          os.mkdir(path)    
+          os.mkdir(path)   
+
+
+def copy_and_overwrite (source,dest):
+     if os.path.exists(dest):
+        shutil.rmtree(dest)
+     shutil.copytree(source,dest)
 
 
 def main (source,target):
      cwd= os.getcwd()
-     source = os.path.join(cwd,source)
-     target = os.path.join(cwd,target)
-     game_paths = find_all_game_paths(source)
-     create_dir(target)
+     source_path = os.path.join(cwd,source)
+     target_path = os.path.join(cwd,target)
+     game_paths = find_all_game_paths(source_path)
+     new_game_dirs = get_name_from_paths(game_paths,"game")
+     create_dir(target_path)
+
+     for src,dest in zip(game_paths,new_game_dirs):
+          dest_path = os.path.join(target_path,dest)
+          copy_and_overwrite(src,dest_path)
+     
+     
+     print(new_game_dirs)
 
 if __name__ == "__main__":
      args = sys.argv
