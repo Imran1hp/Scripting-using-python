3e9d1165385f3854b435ebb55d8eab61abaa958e
ali imran <aliimranlaskar123@gmail.com>
2025-07-20 23:52:42 +0530

    add main fun

--- get_game_data.py
+++ get_game_data.py
@@ -5,6 +5,22 @@ from subprocess import PIPE,run
 import sys
 
 
+def main (source,target):
+     cwd= os.getcwd()
+     source = os.path.join(cwd,source)
+     target = os.path.join(cwd,target)
+
+if __name__ == "__main__":
+     args = sys.argv
+     if len(args)!=3:
+          print("You must pass a source directory and a target directory -only.")
+
+
+     source,target = args[1:]    
+     main(source,target) 
+
+
+
 
 
 
