425da56aef3bbccb1a1e4bcb4497b3b35bb154a8
ali imran <aliimranlaskar123@gmail.com>
2025-07-21 00:08:11 +0530

    add create_dir func

--- get_game_data.py
+++ get_game_data.py
@@ -4,11 +4,33 @@ import shutil
 from subprocess import PIPE,run
 import sys
 
+GAME_DIR_PATTERN = "game"
+
+
+
+def find_all_game_paths(source):
+     game_paths =[]
+
+     for root,dirs,files in os.walk(source):
+          for directory in dirs:
+               if GAME_DIR_PATTERN in directory.lower():
+                    path =os.path.join(source,directory)
+                    game_paths.append(path)
+          break
+          
+     return game_paths
+
+def create_dir(path):
+     if not os.path.exists(path):
+          os.mkdir(path)    
+
 
 def main (source,target):
      cwd= os.getcwd()
      source = os.path.join(cwd,source)
      target = os.path.join(cwd,target)
+     game_paths = find_all_game_paths(source)
+     create_dir(target)
 
 if __name__ == "__main__":
      args = sys.argv
